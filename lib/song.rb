7c04fc6293fca6f2f2320a4ae2ec61f9dca478e0
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-06 02:16:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   @@genres = []
   @@artists = []
   
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist, :genre, :count
   
   def initialize (name, artist, genre)
     @name = name
