409da57d03d365ee0ad121d22168bbf190b67444
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-06 02:57:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     artist_count = {} 
     @@artist_count.each do |artist| 
       if artist_count[artist] 
-        artist_count[artist] +=1 
+        artist_count[artist] += 1 
       else
         artist_count[artist] = 1
       end 
